84fe2e2343f5a2e1fb4fd34a25662189f9bddb6e
dipti92 <babeldipti@gmail.com>
2020-09-30 21:06:20 +0530

    Updates

---
+++ precache-manifest.a72774c32cc01b4050f1d20721cad423.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8557dc66b0c1ec371552a589d766cf18",
+    "url": "/gallery/index.html"
+  },
+  {
+    "revision": "594f3034d93a1dd3177c",
+    "url": "/gallery/static/css/main.f2c071b7.chunk.css"
+  },
+  {
+    "revision": "4dd7b927d6fc511dbd58",
+    "url": "/gallery/static/js/2.fc14baf4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gallery/static/js/2.fc14baf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "594f3034d93a1dd3177c",
+    "url": "/gallery/static/js/main.71564247.chunk.js"
+  },
+  {
+    "revision": "1160e6796d671babbaaa",
+    "url": "/gallery/static/js/runtime-main.7b22ee28.js"
+  }
+]);
